8baa4191528c693bc99835913772f888ac2dd014
Myka Martinez <myka@mykaintech.com>
2018-12-26 16:11:39 -0600

    Section 3, Lecture 20
    - Created foreach_loops.php
    - ForEach Loop example

---
+++ foreach_loops.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+
+<?php  
+
+$numbers = [11, 22, 33, 44, 55, 66, 77, 88, 99, 110];
+
+foreach($numbers as $number) {
+	echo "Array returned " . $number . "<br>";
+}
+
+?>
+
+
+	
+</body>
+</html>
