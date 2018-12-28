093693075ca17d9abec44877b50b0d1c8dce4519
Myka Martinez <myka@mykaintech.com>
2018-12-28 17:25:03 -0600

    Section 5, Lecture 29
    - Created string_functions.php
    - String functions examples

---
+++ string_functions.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>String Fucntions</title>
+</head>
+<body>
+	
+
+<?php  
+
+	$string = "Hello User, how are you today?";
+
+	// Length of string
+	echo strlen($string);
+
+	echo "<br>";
+
+	// Convert string to uppercase
+	echo strtoupper($string);
+
+	echo "<br>";
+
+	// Convert string to lowercase
+	echo strtolower($string);
+
+?>
+
+
+	
+</body>
+</html>
