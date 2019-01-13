d74976ad910f55fddae36708f9bdadf9611f290c
Myka Martinez <myka@mykaintech.com>
2019-01-13 17:54:23 -0600

    Section 7, Lecture 53
    - Created header.php
    - Refactored headers from all CRUD files

--- mysql/login_create.php
+++ mysql/login_create.php
@@ -2,6 +2,9 @@
 
 	include "db.php";
 	include "functions.php";
+	include "html/header.php";
+
+	generateHeader("Create");
 
 	if(isset($_POST['submit'])) {
 		
@@ -14,15 +17,6 @@
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Create</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-</head>
 <body>
 	
 
