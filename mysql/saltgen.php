dcddfeaf726ca8500a1acd8a3428cf35ca3b6cda
Myka Martinez <myka@mykaintech.com>
2019-02-10 15:24:07 -0600

    Security Update
    - Rewrote salt generator to produce a character string using cryptographically secure pseudo-random bytes

--- mysql/saltgen.php
+++ mysql/saltgen.php
@@ -1,16 +1,21 @@
 <?php  
 
 	function saltGen() {
-		$chars 			= "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
-		$charsArray 	= str_split($chars);
-		$arrayLen		= sizeof($charsArray) - 1;
-		$salt           = "";
+		// $chars 			= "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+		// $charsArray 	= str_split($chars);
+		// $arrayLen		= sizeof($charsArray) - 1;
+		// $salt           = "";
 
-		for($saltChar = 0; $saltChar <= 32; $saltChar++) {
-			$index 	  = rand(0, $arrayLen);
-			$randChar = $charsArray[$index];
-			$salt = $salt . $randChar;
-		}
+		$secureBytes = random_bytes(32);
+		$salt = bin2hex($secureBytes);
+
+		// for($saltChar = 0; $saltChar <= 32; $saltChar++) {
+		// 	$index 	  = rand(0, $arrayLen);
+		// 	$randChar = $charsArray[$index];
+		// 	$salt = $salt . $randChar;
+		// }
+
+
 
 		return $salt;
 
